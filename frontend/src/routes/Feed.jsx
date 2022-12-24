97aede8598b78620039f50b22d4c0ea2491cf260
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-12-24 23:20:18 +0200

    adding background and text

--- frontend/src/routes/Feed.jsx
+++ frontend/src/routes/Feed.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {  ReactComponent as Cover } from '../assets/svg/doctor1.svg'
+import {  ReactComponent as Cover2  } from '../assets/svg/giving-pills.svg'
+import '../styles/css/feed.css'
 
 const Feed = () => {
     return (
-        <div className="container">ffff</div>
+        <div>
+                <div> 
+                <Cover className='cover'/>    
+                    <h1 className='cover-text-background'>
+                     Making Medication <br /> Accessible For Everyone
+                    </h1>
+                </div>
+                <div>
+                <Cover2 className='cover2'/> 
+                    <h1 className='cover2-text-background'>
+                      MAKING THE <br />WORLD A<br /> HEALTHIER, <br />HAPPIER PLACE
+                    </h1>
+                </div>
+        </div> 
+         
     )
 }
 
