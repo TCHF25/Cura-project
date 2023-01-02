fa7baedee11ffbc6e0f59640f1f5ecdf82bf9789
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2023-01-02 15:30:27 +0200

    register page setup

--- frontend/src/routes/Register.jsx
+++ frontend/src/routes/Register.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-import { ReactComponent as NavBrand } from "../assets/svg/Landing-Home-Logo.svg";
 import { Link } from "react-router-dom";
 import "../styles/css/auth.css";
+
 
 const Register = () => {
   return (
     <div className="signup-section">
       <div className="signup-wrapper">
         <nav className="signup-nav">
-          <span className="navLogo">
-            <NavBrand className="logo" />
-          </span>
+         
         </nav>
-        <h1 className="form-title">Make the most of your professional life</h1>
         <form className="signup-form">
           <div className="form-groupa">
             <label className="form-label" id="email" htmlFor="email">
-              Email or phone number
+              Email
             </label>
             <input
               type="text"
