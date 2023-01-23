267cc8654be8449d925e94efd6b3fb6c91b99990
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2023-01-23 21:34:32 +0200

    adding a title

--- frontend/src/routes/Profile.jsx
+++ frontend/src/routes/Profile.jsx
@@ -9,10 +9,28 @@ const Profile = () => {
                 <div>
                     <Health className="health-cover"/>
                     <h1 className='health-profile-title'>HEALTH PROFILE</h1>
-                </div>
-                
-                     
-                
+                    <div className="general-infos">
+                        <div>
+                            <h1>General Information</h1>
+                        </div>
+                        <div className="health-inputs">
+                            <div className="left-part">
+                                <div className="full-name">
+                                    <input type="text" />
+                                </div>
+                                <div className="email">
+                                    <input type="text" />
+                                </div>
+                                <div className="date-of-birth">
+                                    <input type="date" />
+                                </div>
+                            </div>
+                            <div className="right-part">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>                         
             </div>
         </div>
     )
