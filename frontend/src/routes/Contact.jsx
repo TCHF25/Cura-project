5ec33cb7d82f0a832a34457b026cbd24dc7923ec
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2023-01-28 19:56:49 +0200

    setting up the contact us page

---
+++ frontend/src/routes/Contact.jsx
@@ -0,0 +1,12 @@
+import React   from "react";
+import '../styles/css/contact.css'
+
+const Contact = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Contact
