6769bd92ed2cefa5c09e83d8625105e49f4859ff
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2023-01-29 22:55:28 +0200

    adding google maps api

--- frontend/src/routes/Contact.jsx
+++ frontend/src/routes/Contact.jsx
@@ -1,10 +1,12 @@
 import React   from "react";
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import '../styles/css/contact.css'
 
 const Contact = () => {
     return (
         <div>
-            
+
         </div>
     )
 }
