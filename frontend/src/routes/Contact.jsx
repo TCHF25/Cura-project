d22a8ac0c7e31045c4309b9ac402b56af3d64f11
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2023-01-30 19:59:06 +0200

    adding google maps api and styling

--- frontend/src/routes/Contact.jsx
+++ frontend/src/routes/Contact.jsx
@@ -1,14 +1,24 @@
 import React   from "react";
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import '../styles/css/contact.css'
+import '../styles/css/globals.css'
 
 const Contact = () => {
-    return (
-        <div>
 
-        </div>
-    )
+    const {isLoaded} = useLoadScript({
+        googleMapsApiKey: "AIzaSyDzrxUHzhJDcAA3ggaFMLQ9ITPhR8qhvOc"});
+
+
+    if (!isLoaded) return <div>Loading...</div>    
+    return (<Map/>)
+
+    function Map() {
+        return <GoogleMap
+         zoom={10}
+         center={{lat: 44, lng: -80}}
+         mapContainerClassName="map-container"
+         ></GoogleMap>
+    }
 }
 
 export default Contact
