a554fbc3e3cd3e62e3099f298c3c44cc2c77a678
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2023-01-31 14:54:47 +0200

    editing google maps api

--- frontend/src/routes/Contact.jsx
+++ frontend/src/routes/Contact.jsx
@@ -1,24 +1,35 @@
 import React   from "react";
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import '../styles/css/globals.css'
+import '../styles/css/contact.css'
 
 const Contact = () => {
 
     const {isLoaded} = useLoadScript({
         googleMapsApiKey: "AIzaSyDzrxUHzhJDcAA3ggaFMLQ9ITPhR8qhvOc"});
-
-
     if (!isLoaded) return <div>Loading...</div>    
-    return (<Map/>)
 
     function Map() {
         return <GoogleMap
          zoom={10}
          center={{lat: 44, lng: -80}}
-         mapContainerClassName="map-container"
-         ></GoogleMap>
+         mapContainerClassName="map-container">   
+         </GoogleMap>
     }
+
+
+    return (
+        
+            <div>
+                <div className="map-background">
+                    <div className="map-title">
+                        <h1 className="map-title">Contact us</h1>
+                    </div>
+                    
+                </div>      
+            </div>
+
+    )    
 }
 
 export default Contact
