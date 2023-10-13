cd2fa5b94824dc4609f738ca3e424127f8ffe688
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2023-10-13 16:08:29 +0300

    updating dependencies

--- frontend/src/routes/Contact.jsx
+++ frontend/src/routes/Contact.jsx
@@ -1,6 +1,5 @@
 import React   from "react";
-import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import '../styles/css/contact.css'
 
 const Contact = () => {
